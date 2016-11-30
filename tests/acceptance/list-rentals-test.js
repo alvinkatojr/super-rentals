4b8123b92f7e5821185955bc65d0dbc68353c3be
alvinkatojr <alvinkatojr@gmail.com>
2016-11-30 19:12:24 +0300

    Adds new tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -10,3 +10,21 @@ moduleForAcceptance('Acceptance | list rentals');
 //     assert.equal(currentURL(), '/');
 //   });
 // });
+
+test('should redirect to rentals route.', function(assert){
+});
+
+test('should list available rentals.', function(assert){
+});
+
+test('should link to information about the company.', function(){
+});
+
+test('should link to contact information.', function(assert){
+});
+
+test('should filter the list of rentals by city.', function(assert){
+});
+
+test('should show details for a specific rental.', function(assert){
+});
