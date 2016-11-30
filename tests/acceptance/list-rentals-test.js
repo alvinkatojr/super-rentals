50f5e8555acbb9f6d701097e90b13fcbabe7ee34
alvinkatojr <alvinkatojr@gmail.com>
2016-11-30 22:18:53 +0300

    Displays details of individual rental test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -52,4 +52,11 @@ test('should filter the list of rentals by city.', function(assert){
 });
 
 test('should show details for a specific rental.', function(assert){
+  visit('/rentals');
+  click('a:contains("Grand Old Mansion")');
+  andThen(function(){
+    assert.equal(currentURL(), '/rentals/grand-old-mansion', 'should navigate to show route')
+    assert.equal('.show-listing h2').text(), "Grand Old Mansion", 'should list rental title');
+    assert.equal('.description').length, 1, 'should list a description of the property');
+  });
 });
