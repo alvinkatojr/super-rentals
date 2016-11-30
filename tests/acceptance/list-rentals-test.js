019a4844c6bf4d07f7c1c1b87cbc62986385eb8c
alvinkatojr <alvinkatojr@gmail.com>
2016-11-30 22:52:38 +0300

    Booboo fiesta..missing method call

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -56,7 +56,7 @@ test('should show details for a specific rental.', function(assert){
   click('a:contains("Grand Old Mansion")');
   andThen(function(){
     assert.equal(currentURL(), '/rentals/grand-old-mansion', 'should navigate to show route');
-    assert.equal('.show-listing h2').text(), "Grand Old Mansion", 'should list rental title');
-    assert.equal('.description').length, 1, 'should list a description of the property');
+    assert.equal(find('.show-listing h2').text(), "Grand Old Mansion", 'should list rental title');
+    assert.equal(find('.description').length, 1, 'should list a description of the property');
   });
 });
