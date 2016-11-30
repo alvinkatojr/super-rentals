29fa8bffa08dd38064d1f98cc0248de8aceab583
alvinkatojr <alvinkatojr@gmail.com>
2016-11-30 20:26:09 +0300

    Confirms clicks to contact and about pages test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -26,9 +26,19 @@ test('should list available rentals.', function(assert){
 });
 
 test('should link to information about the company.', function(){
+  visit('/');
+  click('a:contains("About")');
+  andThen(function(){
+    assert.equal(currentURL(), '/about', 'should navigate to about'
+  });
 });
 
 test('should link to contact information.', function(assert){
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function(){
+    assert.equal(currentURL(), '/contact', 'should navigate to contact'
+  });
 });
 
 test('should filter the list of rentals by city.', function(assert){
