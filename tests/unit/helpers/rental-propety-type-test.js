04033a5ba0e788ecebdcf33e79b6ccd45f493619
alvinkatojr <alvinkatojr@gmail.com>
2016-12-12 19:12:55 +0300

    Creates helper file

---
+++ tests/unit/helpers/rental-propety-type-test.js
@@ -0,0 +1,12 @@
+
+import { rentalPropetyType } from 'super-rentals/helpers/rental-propety-type';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | rental propety type');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = rentalPropetyType([42]);
+  assert.ok(result);
+});
+
