d790083e130c0eb1550a65900ec8dd99c69a2fa3
alvinkatojr <alvinkatojr@gmail.com>
2016-12-15 04:57:19 +0300

    Plain old housekeeping

--- mirage/config.js
+++ mirage/config.js
@@ -27,42 +27,42 @@ export default function() {
   this.namespace = '/api';
 
   let rentals = [{
-      type: 'rentals',
-      id: 'grand-old-mansion',
-      attributes: {
-        title: 'Grand Old Mansion',
-        owner: 'Veruca Salt',
-        city: 'San Francisco',
-        type: 'Estate',
-        bedrooms: 15,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
-        descriptions: "This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests."
-      }
-    }, {
-      type: 'rentals',
-      id: 'urban-living',
-      attributes: {
-        title: 'Urban Living',
-        owner: 'Mike Teavee',
-        city: 'Seattle',
-        type: 'Estate',
-        bedrooms: 15,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
-        descriptions: "A commuters dream. This rental is within walking distance of 2 bus stops and the Metro."
-      }
-    }, {
-      type: 'rentals',
-      id: 'downtown-charm',
-      attributes: {
-        title: 'Grand Old Mansion',
-        owner: 'Violet Beauregarde',
-        city: 'Portland',
-        type: 'Apartment',
-        bedrooms: 15,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
-        description: "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet."
-        }
-      }];
+    type: 'rentals',
+    id: 'grand-old-mansion',
+    attributes: {
+      title: 'Grand Old Mansion',
+      owner: 'Veruca Salt',
+      city: 'San Francisco',
+      type: 'Estate',
+      bedrooms: 15,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+      descriptions: "This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests."
+    }
+  }, {
+    type: 'rentals',
+    id: 'urban-living',
+    attributes: {
+      title: 'Urban Living',
+      owner: 'Mike Teavee',
+      city: 'Seattle',
+      type: 'Estate',
+      bedrooms: 15,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
+      descriptions: "A commuters dream. This rental is within walking distance of 2 bus stops and the Metro."
+    }
+  }, {
+    type: 'rentals',
+    id: 'downtown-charm',
+    attributes: {
+      title: 'Grand Old Mansion',
+      owner: 'Violet Beauregarde',
+      city: 'Portland',
+      type: 'Apartment',
+      bedrooms: 15,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
+      description: "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet."
+    }
+  }];
 
   this.get('/rentals', function(db, request){
     if (request.queryParams.city !== undefined){
