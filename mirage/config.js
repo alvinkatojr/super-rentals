82fce02cb3f48904b99c241f2b9502fd97a324ba
alvinkatojr <alvinkatojr@gmail.com>
2016-12-15 04:46:50 +0300

    Updates mirage to respond to queries

--- mirage/config.js
+++ mirage/config.js
@@ -64,4 +64,14 @@ export default function() {
         }
       }];
 
+  this.get('/rentals', function(db, request){
+    if (request.queryParams.city !== undefined){
+      let filteredRentals = rentals.filter(function(i){
+        return i.attributes.city.toLowerCase().indexOf(request.queryParams.city.toLowerCase()) !== -1;
+      });
+      return { data: filteredRentals };
+    } else {
+      return { data: rentals };
+    }
+  });
 }
