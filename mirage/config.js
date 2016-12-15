2c72283a9227d2761c3791718576896ff896a118
alvinkatojr <alvinkatojr@gmail.com>
2016-12-15 04:39:23 +0300

    Adds descriptions

--- mirage/config.js
+++ mirage/config.js
@@ -36,6 +36,7 @@ export default function() {
         type: 'Estate',
         bedrooms: 15,
         image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+        descriptions: "This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests."
       }
     }, {
       type: 'rentals',
@@ -47,6 +48,7 @@ export default function() {
         type: 'Estate',
         bedrooms: 15,
         image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+        descriptions: "A commuters dream. This rental is within walking distance of 2 bus stops and the Metro."
       }
     }, {
       type: 'rentals',
@@ -58,6 +60,7 @@ export default function() {
         type: 'Apartment',
         bedrooms: 15,
         image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+        description: "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet."
         }
       }];
 
