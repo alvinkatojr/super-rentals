7a7c44c7e08a09db18cc74bb3dc7e4c03dcc6064
alvinkatojr <alvinkatojr@gmail.com>
2016-12-18 04:46:24 +0300

    Creates map util stub

--- tests/unit/services/maps-test.js
+++ tests/unit/services/maps-test.js
@@ -1,8 +1,19 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
+
+const DUMMY_ELEMENT = {};
+
+let MapUtilStub = Ember.Object.extend({
+  createMap(element, location){
+    this.assert.ok(element, 'createMap called with element');
+    this.assert.ok(location, 'createMap called with location');
+    return DUMMY_ELEMENT;
+  }
+})
 
 moduleFor('service:maps', 'Unit | Service | maps', {
   // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+  needs: ['util:google-maps']
 });
 
 // Replace this with your real tests.
