0774d9c1b170b2d95d2b67880df257114d154dd0
alvinkatojr <alvinkatojr@gmail.com>
2016-12-19 23:44:26 +0300

    Another initializer function, geocoder set

--- app/utils/google-maps.js
+++ app/utils/google-maps.js
@@ -1,3 +1,11 @@
-export default function googleMaps() {
-  return true;
-}
+import Ember from 'ember';
+
+const google = window.google;
+
+export default Ember.Object.extend({
+
+  init(){
+    this.set('geocoder', new google.maps.GeoCoder());
+  },
+
+});
