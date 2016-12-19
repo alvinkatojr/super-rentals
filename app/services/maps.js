94aea942524ef942a370f874eedff563d831e9af
alvinkatojr <alvinkatojr@gmail.com>
2016-12-19 23:14:17 +0300

    And here comes our initializer function

--- app/services/maps.js
+++ app/services/maps.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
+import MapUtil from '../utils/google-maps';
 
 export default Ember.Service.extend({
+
+  init(){
+    if (!this.get('cachedMaps')){
+      this.set('cachedMaps', Ember.Object.create());
+    }
+    if (!this.get('mapUtil')){
+      this.set('mapUtil', MapUtil.create());
+    }
+  },
 });
