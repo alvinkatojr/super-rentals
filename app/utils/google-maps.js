fb60b82404155841a4f3eefb67a52ceae1bf40b7
alvinkatojr <alvinkatojr@gmail.com>
2016-12-20 00:01:40 +0300

    Adds pin location function

--- app/utils/google-maps.js
+++ app/utils/google-maps.js
@@ -14,4 +14,15 @@ export default Ember.Object.extend({
     return map;
   },
 
+  pinLocation(location, map){
+    this.get('geocoder').geocode({address: location}, (result, status) => {
+      if (status === google.maps.GeocoderStatus.OK){
+        let geometry = result[0].geometry.location;
+        let position = { lat: geometry.lat(), lng: geometry.lng() };
+        map.setCenter(position);
+        new google.maps.Marker({ position, map, title: location });
+      }
+    });
+  }
+
 });
