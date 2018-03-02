5458bf9420c49f23f0dd45c5cd07a1734e1b6a16
alvinkatojr <alvinkatojr@gmail.com>
2018-03-02 11:23:03 +0300

    Tests for filtering of input values on keyup

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -36,6 +36,42 @@ module('Integration | Component | list-filter', function(hooks) {
     });
   });
 
+  test('should update with matching listings', function(assert){
+    this.on('filterByCity', (val) => {
+      if (val === ''){
+        return RSVP.resolve({
+          query: val,
+          results: ITEMS
+        });
+      } else {
+        return RSVP.resolve({
+          query: val,
+          results: FILTERED_ITEMS
+        });
+      }
+    });
+
+    this.render(hbs`
+      {{#list-filter filter=(action 'filterByCity) as |results|}}
+        <ul>
+        {{#each results as |item|}}
+          <li class="city">
+            {{item.city}}
+          </li>
+        {{/each}}
+        </ul>
+      {{/list-filter}}
+    `);
+
+    // The keyup event here should invoke an action that will cause the list to be filtered
+    this.$('.list-filter input').value('San').keyup();
+    
+    return wait().then(() => {
+      assert.equal(this.$('city').length, 1);
+      assert.equal(this.$('.city').text().trim(), 'San Francisco');
+    });
+  });
+
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
